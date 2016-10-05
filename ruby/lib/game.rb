897f098f118a5643b722e09a51cfefe49b4077a4
John Wilger <john@johnwilger.com>
2016-10-05 06:12:07 -0700

    Paired with Lachlan
    We spent most of our time thinking about what structure to use in order to store
    the grid and cell-state. We settled on using a Set that would contain Cell
    objects. A Cell object would be identified by its coordinates, and any two Cell
    objects with the same coordinates would be considered equal. In this way, we
    guarantee that no two cells in the Set would have the same coordinates. Each
    cell would then be told to transform on each generation, and it would be able to
    find the state of each adjacent cell by performing the coordinate math based on
    its own coordinates.

--- ruby/lib/game.rb
+++ ruby/lib/game.rb
@@ -1,3 +1,13 @@
 class Game
+  attr_accessor :cells
 
+  def initialize
+    self.cells = []
+    1000.times do
+      cells << Cell.new
+    end
+  end
+
+  class Cell
+  end
 end
--- ruby/spec/lib/game_spec.rb
+++ ruby/spec/lib/game_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 require 'game'
 
 describe Game do
-  it 'exists' do
+  it 'has a game board with 1000 cells' do
+    expect(subject.cells.size).to eq 1000
+  end
+
+  it 'has a game board where each cell is an instance of Cell' do
+    expect(subject.cells.all? { |c| c.kind_of?(Game::Cell) }).to be true
+  end
+
+  it 'has a cell for each coordintate in a 100x100 square' do
+    100.times do |x|
+      100.times do |y|
+        expect(subject.cell_at(x,y)).to eq Cell.new(x: x, y: y)
+      end
+    end
   end
 end
